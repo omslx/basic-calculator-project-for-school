92d2972ae59dc384ba128595515cda4793ef3665
omslx <mslx567@gmail.com>
2025-06-26 20:33:22 +0330

    ....

---
+++ src/Calculate.kt
@@ -0,0 +1,13 @@
+val calculate = { t: List<Int>, x: String ->
+    val result:Int?
+    for (i in 1..30)
+        print("-")
+    if(x.isNotEmpty())
+    when (x) {
+        "+" -> {result =t[0] beezafe t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "-" -> {result =t[0] menha t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "*" -> {result =t[0] zarb t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "/" -> {result =t[0] taghsim t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "%" -> {result =t[0] kharjGhesmat t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+    }
+}
---
+++ src/Math.kt
@@ -0,0 +1,5 @@
+infix fun Int.zarb(other:Int) = this * other
+infix fun Int.beezafe(other:Int) = this + other
+infix fun Int.menha(other:Int) = this - other
+infix fun Int.kharjGhesmat(other:Int) = this % other
+infix fun Int.taghsim(other:Int) = this / other
