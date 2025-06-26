66e5d6f9895546ab513e924348aafa4bd9f427bb
mslx <125800756+omslx@users.noreply.github.com>
2025-06-26 21:30:09 +0330

    Update calculate.kt

--- src/calculate.kt
+++ src/calculate.kt
@@ -1,14 +1,13 @@
-fun calculate(t: List<Int>, x: String): Int {
-    var result = 1
+val calculate = { t: List<Int>, x: String ->
+    var result = 0
     for (i in 1..30)
         print("-")
     if(x.isNotEmpty())
     when (x) {
-        "+" -> {result =t[0] + t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
-        "-" -> {result =t[0] - t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
-        "*" -> {result =t[0] * t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
-        "/" -> {result =t[0] / t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
-        "%" -> {result =t[0] % t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "+" -> {result =t[0] beezafe t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "-" -> {result =t[0] menha t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "*" -> {result =t[0] zarb t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "/" -> {result =t[0] taghsim t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
+        "%" -> {result =t[0] kharjGhesmat t[1] ; println("\nThe response of ${t[0]} $x ${t[1]} = $result")}
     }
-    return result
 }
